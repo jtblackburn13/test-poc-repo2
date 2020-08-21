998fc8e8d831e2b6f48e1f4cc35d5f3de2a44f6a
Tyler Blackburn <39885164+jtblackburn13@users.noreply.github.com>
2020-08-21 04:26:33 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-logo">~~~~~VENTURE_NAME~~~~~</h1>
+        <p>
+          ~~~~~TAG_LINE~~~~~
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
